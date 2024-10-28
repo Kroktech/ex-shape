cd4ee98fd421dd39685efc0de4d129cd74de6ebf
Kroktech <ablanchet@gaming.tech>
2024-10-28 20:03:38 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+class Vec2
+{;
+public:
+	float x;
+	float y;
+};
+
+class Shape 
+{
+public:
+	Shape(float x,float y){}
+
+	void show()
+	{
+
+	}
+
+
+
+};
+class Circle : public Shape
+{
+public:
+
+
+};
+class Rectangle : public Shape
+{
+
+
+
+};
+class Triangle : public Shape
+{
+
+};
